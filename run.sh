b5693b55fc5ada35e12cb0e4c891c460e5b360db
aleksaradomirovic <34846410+aleksaradomirovic@users.noreply.github.com>
2024-04-24 14:15:05 -0700

    added run script and made it executable

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 # write a bash script that runs optimizer in this file.
 # script takes one argument, a path to the input ir file 
 # script outputs an optimized ir file named out.ir
+java -cp build IRCompiler ${1} > out.ir
